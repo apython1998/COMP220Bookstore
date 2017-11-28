0fb98f2274bcb09d38e72d75641fc3d5f5475acd
Colin Hay <chay@ithaca.edu>
2017-11-28 10:39:20 -0500

    Adding SortedTitleList .h

--- ArraySortedTitleList.h
+++ ArraySortedTitleList.h
@@ -2,35 +2,39 @@
 // Created by chay on 11/27/2017.
 //
 
-#ifndef COMP220BOOKSTORE_SORTEDTITLELIST_H
-#define COMP220BOOKSTORE_SORTEDTITLELIST_H
+#ifndef COMP220BOOKSTORE_ARRAYSORTEDTITLELIST_H
+#define COMP220BOOKSTORE_ARRAYSORTEDTITLELIST_H
 
-class SortedTitleList {
+class ArraySortedTitleList {
+private:
+    //pointer to start of array
+    ArrayList* sortedTitleList;
+
 public:
     /**
      * Constructor
      * creates a pointer to an empty ArrayList
     */
-    virtual SortedTitleList()=0;
+    SortedTitleList();
 
     /**
      * Copy Constructor
      * shallow copies listToCopy to the ArrayList pointer sortedTitleList
     */
-    virtual SortedTitleList(const Title listToCopy)=0;
+    SortedTitleList(const ArrayList* listToCopy);
 
     /**
      * Destructor
      * frees ArrayList pointer sortedTitleList
     */
-    virtual ~SortedTitleList(){};
+    ~SortedTitleList();
 
     /**
      * finds the location of a Title object in the sortedTitleList using binary search
      * @param titleToFind the Title to find
      * @returns index of Title (-1 if it doesn't exist)
     */
-    virtual int find(Title titleToFind)=0;
+    int find(Title titleToFind);
 
     /**
      * adds the Title object to the sortedTitleList in its alphabetical position
@@ -38,7 +42,7 @@ public:
      * @throws illegal_argument exception if Title already exists in sortedTitleList
      * @post sortedTitleList is in alphabetical order
     */
-    virtual void add(Title titleToAdd)=0;
+    void add(Title titleToAdd);
 };
 
-#endif //COMP220BOOKSTORE_SORTEDTITLELIST_H
+#endif //COMP220BOOKSTORE_ARRAYSORTEDTITLELIST_H
