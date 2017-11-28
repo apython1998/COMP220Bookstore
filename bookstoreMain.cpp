cbd2be326f213ccc33aeab997e7caac59ad203cc
apython1998 <alex.python1998@gmail.com>
2017-11-28 10:42:44 -0500

    Defined methods for UI

--- bookstoreMain.cpp
+++ bookstoreMain.cpp
@@ -2,6 +2,8 @@
 // Created by Alex Python on 11/15/17.
 //
 #include <iostream>
+#include "UI.h"
+#include "InventoryManager.h"
 
 void bookstoreMain() {
     std::cout<< "IN BOOKSTORE MAIN RUN"<< std::endl;
